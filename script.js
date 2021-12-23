9bf1b98ba092d71a131ad7d5ac619d0b7e9e6fea
Daniel-egw <83707476+Daniel-egw@users.noreply.github.com>
2021-12-23 23:32:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let stars = [];
+
+let count = 0;
+while(count <= 9)
+{
+    stars[count] = document.getElementById("star" + (count + 1))
+    count++
+}
+
+setInterval(function ()
+{
+    for(i = 0; i < stars.length; i++)
+    {
+        stars[i].style.color = "rgb(" + randomNumber(255) + ", " + randomNumber(255) + ", " + randomNumber(255) + ")"
+        stars[i].style.left = randomNumber(400) + "px"
+        stars[i].style.top = randomNumber(600) + "px"
+    }
+}, 200)
+
+function randomNumber(max)   //random number 
+{
+    return Math.floor(Math.random() * max)
+}
